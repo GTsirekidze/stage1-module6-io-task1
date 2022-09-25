62549c15c49ee718b5cae2495b4db43e3f29cd75
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-25 13:55:06 +0400

    "commit"

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,8 +2,12 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileReader {
+
+    private final Logger logger =  Logger.getLogger(this.getClass().getName());
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
@@ -27,12 +31,12 @@ public class FileReader {
                         profile.setPhone(Long.valueOf(splittedLine[1]));
                         break;
                     default:
-                        System.err.println("Wrong key");
+                        logger.log(Level.WARNING,"Wrong key");
 
                 }
             }
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            logger.log(Level.WARNING,e.getMessage());
             return null;
         }
 
