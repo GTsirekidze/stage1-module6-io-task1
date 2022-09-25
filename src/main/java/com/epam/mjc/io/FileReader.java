c4467c01e1de5b1a83a0a187d587bb038fd7a844
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-25 13:44:44 +0400

    "commit"

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+                String[] splittedLine = line.split(" ");
+                switch (splittedLine[0]) {
+                    case "Name:":
+                        profile.setName(splittedLine[1]);
+                        break;
+                    case "Age:":
+                        profile.setAge(Integer.parseInt(splittedLine[1]));
+                        break;
+                    case "Email:":
+                        profile.setEmail(splittedLine[1]);
+                        break;
+                    case "Phone:":
+                        profile.setPhone(Long.valueOf(splittedLine[1]));
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return profile;
     }
 }
